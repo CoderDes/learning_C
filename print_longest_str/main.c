1a7e8cc3b43fe18f546399df391c2bcd39d166b4
Eugene Zubkov <eugzubv@gmail.com>
2022-10-31 13:42:50 +0400

    print_longest_str

---
+++ print_longest_str/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define MAX_LINE 1000
+int get_line(char line[], int max_line);
+void copy(char to[], char from[]);
+
+
+int main()
+{
+    char current_line[MAX_LINE];
+    char longest_line[MAX_LINE];
+
+    int current_len = 0;
+    int max_len = 0;
+
+    while ((current_len = get_line(current_line, MAX_LINE)) > 0) {
+        if (current_len > max_len) {
+            max_len = current_len;
+            copy(longest_line, current_line);
+        }
+    }
+
+    short thereWasAtleastOneLine = max_len > 0;
+    if (thereWasAtleastOneLine) {
+        printf("%s", longest_line);
+    }
+
+    return 0;
+}
+
+int get_line(char s[], int lim)
+{
+    int ch, i;
+    for (i = 0; i < lim - 1 && (ch = getchar()) != EOF && ch != '\n'; ++i) {
+        s[i] = ch;
+    }
+
+    if (ch == '\n') {
+        s[i] = ch;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[])
+{
+    int i = 0;
+    while ((to[i] = from[i]) != '\0') {
+        ++i;
+    }
+}
