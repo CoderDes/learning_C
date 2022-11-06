6b5cb45e6cb5f76c405144c37e1d0ba44d0ea0a5
Eugene Zubkov <eugzubv@gmail.com>
2022-11-07 00:50:59 +0400

    refactor 1.19

--- 1.19/main.c
+++ 1.19/main.c
@@ -3,16 +3,21 @@
 #define MAX_LEN 1000
 
 void reverse(char line[], short len);
+void swap(char line[], short i, short j);
+
+void swap(char line[], short i, short j)
+{
+    line[i] += line[j];
+    line[j] = line[i] - line[j];
+    line[i] -= line[j];
+}
 
 void reverse(char line[], short len) {
     short endLineInd = len - 1;
     short startLineInd = 0;
     short middleLineInd = len / 2;
     while (startLineInd != middleLineInd) {
-        line[startLineInd] += line[endLineInd];
-        line[endLineInd] = line[startLineInd] - line[endLineInd];
-        line[startLineInd] -= line[endLineInd];
-        
+        swap(line, startLineInd, endLineInd);
         ++startLineInd;
         --endLineInd;
     }
